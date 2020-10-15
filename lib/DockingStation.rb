34b0ea54720d679cb0e2c1a4996716039ded0c60
Rhianna <rhiannastuart@hotmail.co.uk>
2020-10-15 16:28:09 +0100

    progress?

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,17 +1,21 @@
-
+require_relative 'bike'
 
 class DockingStation 
+
 attr_reader :bike
     def release_bike
         Bike.new
     end
+  
     def dock(bike) 
       @bike = bike
     end
 end
+
 def bike
   @bike
 end
+
 class Bike
     def working?
       true
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,18 +1,19 @@
 require_relative 'bike_spec.rb'
 require 'DockingStation.rb'
 
+
+
 docking_station = DockingStation.new
 RSpec.describe DockingStation do 
     it {is_expected.to respond_to :release_bike}
     it 'releases working bikes' do
-    expect(bike).to be_working
     it {is_expected.to respond_to(:bike)}
     it 'returns docked bikes' do
         bike = Bike.new
         subject.dock(bike)
-        expect(subject.bike).to eq bike
-    end 
-    end 
+        expect(bike).to be_working
+    end
+end
 describe '#release_bike' do
     it {is_expected.to respond_to :release_bike}
     describe 'docking_station'
